20c475c2cda67ece51291c8cabc19d1ecc1e2fee
Nicholas Lam <nlam.intern@fce.com>
2025-12-03 13:50:07 -0700

    Placeholder, need to fix styling.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
 document.getElementById('simulatorbutton').addEventListener('click', function() {
     window.location.href = './simulator';  
 });
+
+document.getElementById('incomebutton').addEventListener('click', function() {
+    window.location.href = './income';  
+});
