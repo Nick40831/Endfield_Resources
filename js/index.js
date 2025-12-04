465ca0afa964b7829fbf6075a5a4e0890c5062c3
Nicholas Lam <nlam.intern@fce.com>
2025-12-03 17:37:21 -0700

    Made standard styles for index page. Need to move to a standard file.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
-document.getElementById('simulatorbutton').addEventListener('click', function() {
-    window.location.href = './simulator';  
+document.addEventListener("DOMContentLoaded", () => {
+	const carousel = document.getElementById("tools-carousel");
+	const btnLeft = document.getElementById("carousel-left");
+	const btnRight = document.getElementById("carousel-right");
+
+	const scrollAmount = 200; 
+
+	btnLeft.addEventListener("click", () => {
+		carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+	});
+
+	btnRight.addEventListener("click", () => {
+		carousel.scrollBy({ left: scrollAmount, behavior: "smooth" });
+	});
+});
+
+document.getElementById('simulator-button').addEventListener('click', function() {
+	window.location.href = './simulator';  
 });
