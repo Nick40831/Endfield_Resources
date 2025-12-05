4fd5e3cb43743001ab58866915fd3a10ceaa5769
Nick40831 <nick40831@hotmail.com>
2025-12-05 12:42:49 -0700

    Added landmine widget. Looking to learn how to implement cookies.

--- js/landmine.js
+++ js/landmine.js
@@ -1,1 +1,28 @@
- 
+function loadMine() {
+	const button = document.createElement('button')
+	const mine = document.createElement('img');
+
+	button.setAttribute('style', 'cursor: pointer;')
+	button.onclick = explodeMine;
+
+	mine.id = "landmine"
+	mine.src = "../images/Landmine.png"
+	mine.setAttribute('style', 'position: absolute; right: 30px; bottom: 55px; width: 3rem; height: auto; transform: rotate(-15deg);');
+
+	button.appendChild(mine)
+	document.body.appendChild(button)
+}
+
+function explodeMine(event) {
+	if (Math.random() < 0.9) {
+		const mine = event.target;
+		let cookie = document.cookie;
+		console.log(cookie);
+		mine.src = "../images/Landmine_Exploded.png"; 
+		setTimeout(() => {
+			mine.src = "../images/Landmine.png";
+		}, 500); 
+	}
+}
+
+loadMine()
