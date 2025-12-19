47ea367f53098cf58113e9761bdef3bb047b3423
Nick40831 <nick40831@hotmail.com>
2025-12-18 19:34:51 -0700

    Initialize React Project

--- js/standard.js
+++
@@ -1,8 +0,0 @@
-import { insertComponent } from "../js/components.js";
-import { loadMine } from "../js/mine.js"
-import { loadUserButton } from "../js/title.js"
-
-insertComponent("#load-title", "../components/title.html", loadUserButton);
-insertComponent("#load-footer", "../components/footer.html");
-
-loadMine();
